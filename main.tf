b6c3cceefd78b36f8a0a3eb70ee42325a69ef844
Binge-Plus <bingemoviess@gmail.com>
2024-09-27 12:14:06 +0530

    secrets removed

--- main.tf
+++ main.tf
@@ -36,12 +36,12 @@ variable "project_id" {
 
 variable "region" {
   description = "The region to deploy the instance"
-  default     = "us-central1"
+  # default     = "us-central1"
 }
 
 variable "zone" {
   description = "The zone to deploy the instance"
-  default     = "us-central1-a"
+  # default     = "us-central1-a"
 }
 
 variable "instance_name" {
@@ -67,14 +67,14 @@ variable "disk_size" {
 variable "ssh_username" {
   description = "Username for SSH access"
   type        = string
-  default     = "binge+"
+  # default     = "binge+"
 }
 
 variable "ssh_public_key" {
   description = "Public SSH key for the user"
   type        = string
   # You can add your Desired
-  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDbmKHGqzplfeRywv01nUHiOXUzJehNTHwvO3OS6SUTU9rQPN3QHSBLWX4tcOqGe5VXfUFMZ89flajPvI4EUmde/cv75Le8jreymWx9ZV4KjEt7KZjKqyjjC7lI7ko2cGtIpSSmoAwob0p5RhXQ2szUJTGfW6qeOsNK9BKREbaEFGoZnD/8O1vEibU0choOLskkMknBJxWzFbOa3Cd5/QR7drg66wTzmfa5mQgPtSnlrVaqI83zz8JydTTuf0jiruUB3XIqRnB0bM5q25WHTNQd2SdulEB6wSu6UDVWtPwSPMyw/BNXpd5ggHX0Aq12asSt5cRtRgcf8aOxrt5VtiHb binge+"
+  # default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDbmKHGqzplfeRywv01nUHiOXUzJehNTHwvO3OS6SUTU9rQPN3QHSBLWX4tcOqGe5VXfUFMZ89flajPvI4EUmde/cv75Le8jreymWx9ZV4KjEt7KZjKqyjjC7lI7ko2cGtIpSSmoAwob0p5RhXQ2szUJTGfW6qeOsNK9BKREbaEFGoZnD/8O1vEibU0choOLskkMknBJxWzFbOa3Cd5/QR7drg66wTzmfa5mQgPtSnlrVaqI83zz8JydTTuf0jiruUB3XIqRnB0bM5q25WHTNQd2SdulEB6wSu6UDVWtPwSPMyw/BNXpd5ggHX0Aq12asSt5cRtRgcf8aOxrt5VtiHb binge+"
 }
 
 # Outputs
