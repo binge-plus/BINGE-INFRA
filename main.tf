1a0466f9667e2577a1108c82dae5bfe4f7f8d211
ankushjha-aj <jhaankushcpp@gmail.com>
2025-04-12 09:17:14 +0530

    Adding Admim Server

--- main.tf
+++ main.tf
@@ -16,6 +16,17 @@ module "gcp_instance" {
   ssh_public_key = var.ssh_public_key
 }
 
+module "binge-plus-admin-server" {
+  source         = "./modules/gcp_instance"
+  instance_name  = "binge-plus-admin"
+  machine_type   = var.machine_type
+  zone           = var.zone
+  image          = var.image
+  disk_size      = var.disk_size
+  ssh_username   = var.ssh_username
+  ssh_public_key = var.ssh_public_key
+}
+
 module "firewall" {
   source     = "./modules/firewall"
   project_id = var.project_id
