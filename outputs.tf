66529c1e91fd6683784817eeef0cac2467f1422c
ankushjha-aj <jhaankushcpp@gmail.com>
2025-04-12 09:34:07 +0530

    Adding Admim Server

--- outputs.tf
+++ outputs.tf
@@ -4,5 +4,13 @@ output "instance_name" {
 }
 
 output "instance_external_ip" {
-  value = module.gcp_instance.instance_external_ip
+  value = module.gcp_instance.instance_external_ip 
+} 
+
+output "admin-instance_name" {
+  value = module.binge-plus-admin-server.instance_name
+}
+
+output "admin-instance_external_ip" {
+  value = module.binge-plus-admin-server.instance_external_ip
 } 
