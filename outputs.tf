2aa7ac26e5ff649401fd9fc5da0ac5942808df8d
ankushjha-aj <jhaankushcpp@gmail.com>
2025-04-15 14:28:24 +0530

    minor RND

--- outputs.tf
+++ outputs.tf
@@ -7,10 +7,10 @@ output "instance_external_ip" {
   value = module.gcp_instance.instance_external_ip 
 } 
 
-output "admin-instance_name" {
-  value = module.binge-plus-admin-server.instance_name
-}
+# output "admin-instance_name" {
+#   value = module.binge-plus-admin-server.instance_name
+# }
 
-output "admin-instance_external_ip" {
-  value = module.binge-plus-admin-server.instance_external_ip
-} 
+# output "admin-instance_external_ip" {
+#   value = module.binge-plus-admin-server.instance_external_ip
+# } 
