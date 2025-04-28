b777dc3328da71e7df4a9732d0ca436245191314
ankushjha-aj <jhaankushcpp@gmail.com>
2025-04-28 10:34:00 +0530

    Setup New Infra

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,16 @@
 # Outputs
 output "instance_name" {
-  value = module.gcp_instance.instance_name
+  value = module.binge-plus-server.instance_name
 }
 
 output "instance_external_ip" {
-  value = module.gcp_instance.instance_external_ip
+  value = module.binge-plus-server.instance_external_ip
 }
 
-# output "admin-instance_name" {
-#   value = module.binge-plus-admin-server.instance_name
-# }
+output "admin-instance_name" {
+  value = module.binge-plus-admin-server.instance_name
+}
 
-# output "admin-instance_external_ip" {
-#   value = module.binge-plus-admin-server.instance_external_ip
-# } 
+output "admin-instance_external_ip" {
+  value = module.binge-plus-admin-server.instance_external_ip
+} 
