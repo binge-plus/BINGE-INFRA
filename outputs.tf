68026bbb62930895e4e2ca2f202f4fbf340210b0
Binge-Plus <bingemoviess@gmail.com>
2025-07-17 00:25:33 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,18 @@
 # Outputs
 output "instance_name" {
-  value = module.binge-plus-server.instance_name
+  value = module.binge-plus-fe-server.instance_name
 }
 
 output "instance_external_ip" {
-  value = module.binge-plus-server.instance_external_ip
+  value = module.binge-plus-fe-server.instance_external_ip
+}
+
+output "instance_name" {
+  value = module.binge-plus-be-server.instance_name
+}
+
+output "instance_external_ip" {
+  value = module.binge-plus-be-server.instance_external_ip
 }
 
 output "admin-instance_name" {
